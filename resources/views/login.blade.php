11438f6cf6e3e0e0948f3e15ae434226b4c77951
Clément Le Goffic <clementlegoffic@gmail.com>
2025-02-17 14:29:58 +0100

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Connexion</title>
+</head>
+<body>
+
+    <main>
+        <section>
+            <div class="container">
+                <h1>Connectez-vous !</h1>
+                <ul>
+                    <li><a>Pseudo</a></li>
+                    <li><a>password</a></li>
+                    <li><a>confirmpassword</a></li>
+                </ul>
+            </div>
+        </section>
+    </main>
+
+</body>
+</html>
